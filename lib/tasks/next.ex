24afad01c07650ee1501f75d31cc85c90ff0c986
Martin Gausby <martin@gausby.dk>
2015-07-08 11:26:33 +0200

    will now copy the exercise files from exercise 1 when executing the workshop.next function

--- lib/tasks/next.ex
+++ lib/tasks/next.ex
@@ -1,7 +1,35 @@
 defmodule Mix.Tasks.Workshop.Next do
   use Mix.Task
+  import Mix.Generator
 
   def run(_) do
-    # todo, copy next exercise folder to sandbox
+    next_exercise = find_next_exercise()
+    source = "./sample/#{next_exercise}/files"
+    destination = "./#{next_exercise}"
+
+    case File.mkdir(destination) do
+      :ok ->
+        copy_exercise_files_to_sandbox(source, destination)
+        Mix.shell.info "Exercise files written to #{destination}"
+      {:error, :eexist} ->
+        Mix.shell.info "Exercise folder #{destination} already exist"
+    end
+  end
+
+  defp find_next_exercise do
+    "exercise_1"
+  end
+
+  defp copy_exercise_files_to_sandbox(source, destination) do
+    Enum.each(File.ls!(source), fn item ->
+      if File.dir? item do
+        new_destination = Path.join(destination, item)
+        create_directory(new_destination)
+        copy_exercise_files_to_sandbox(Path.join(source, item), new_destination)
+      else
+        content = File.read!(Path.join(source, item))
+        create_file(Path.join(destination, item), content)
+      end
+    end)
   end
 end
