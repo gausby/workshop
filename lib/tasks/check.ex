eacf94836ddf7443afbb543b026664026a24394d
Martin Gausby <martin@gausby.dk>
2015-08-02 21:13:05 +0200

    The check script will now find the exercise folder

--- lib/tasks/check.ex
+++ lib/tasks/check.ex
@@ -3,8 +3,17 @@ defmodule Mix.Tasks.Workshop.Check do
 
   @spec run(OptionParser.argv) :: :ok
   def run(argv) do
+    Workshop.start([], [])
     {opts, _, _} = OptionParser.parse(argv, switches: [system: :boolean])
 
-    # find current exercise and run the acceptance test
+    current_exercise = Workshop.State.get(:progress)
+    if current_exercise do
+      current_exercise_folder = Workshop.State.get(:progress)[:cursor]
+                                |> Path.expand(Workshop.Session.get(:exercises_folder))
+
+      # todo, run acceptance test
+    else
+      Mix.shell.info "The workshop has not been started yet"
+    end
   end
 end
