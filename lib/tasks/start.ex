561cc67f75fd93e91fee84f12db08453686e1251
Martin Gausby <martin@gausby.dk>
2015-08-07 11:46:34 +0200

    Added a `start` task for starting a workshop
    Will run the two validators, first Doctor (system validation), then
    workshop validation; if both pass it will show the description of the
    workshop.
    
    The success message will get replaced with a workshop introduction with
    a message about what is next.

---
+++ lib/tasks/start.ex
@@ -0,0 +1,62 @@
+defmodule Mix.Tasks.Workshop.Start do
+  use Mix.Task
+  alias IO.ANSI.Docs
+
+  @spec run(OptionParser.argv) :: :ok
+  def run(argv) do
+    Workshop.start([], [])
+
+    handle_result(cond do
+      fail_doctor? ->
+        {:error, """
+        The system does not yet fit the requirements for this workshop.
+
+        Please run the command:
+
+          `mix workshop.doctor`
+
+        To get a report on what is missing.
+        """}
+      fail_workshop_validation? ->
+        {:error, """
+        This does not appear to be a valid workshop. Please run the command:
+
+          `mix workshop.validate`
+
+        This will display the validation results.
+        """}
+      :otherwise ->
+        :ok
+    end)
+  end
+
+  defp handle_result(:ok) do
+    Docs.print_heading Workshop.Meta.info[:title]
+    Docs.print Workshop.Meta.info[:description]
+  end
+  defp handle_result({:error, reason}) do
+    Mix.shell.error reason
+    System.at_exit fn _ ->
+      exit({:shutdown, 1})
+    end
+  end
+
+  # wrappers for validators
+  defp fail_doctor? do
+    case Workshop.doctor do
+      %{runs: x, passed: x} ->
+        false
+      _ ->
+        true
+    end
+  end
+
+  defp fail_workshop_validation? do
+    case Workshop.validate do
+      %{runs: x, passed: x} ->
+        false
+      _ ->
+        true
+    end
+  end
+end
