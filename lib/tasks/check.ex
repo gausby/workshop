af60eae3a37fef282a23cb709cc3160cc2f8fd88
Martin Gausby <martin@gausby.dk>
2015-08-25 11:51:51 +0200

    Just mark exercise as completed when checking
      - This is temporary, it should run the acceptance test for the current
        exercise and set it to `:completed` if it passes, `:in_progress` if
        it fails

--- lib/tasks/check.ex
+++ lib/tasks/check.ex
@@ -1,6 +1,6 @@
 defmodule Mix.Tasks.Workshop.Check do
   use Mix.Task
-
+  alias Workshop.Exercise
   @spec run(OptionParser.argv) :: :ok
   def run(argv) do
     Workshop.start([], [])
@@ -8,7 +8,17 @@ defmodule Mix.Tasks.Workshop.Check do
 
     current_exercise = Workshop.Session.get(:current_exercise)
     if current_exercise do
-      Mix.shell.info "todo, run acceptance test!"
+      exercise = Exercise.load current_exercise
+      exercises_state = Workshop.State.get(:exercises, [])
+      identifier = Exercise.get_identifier(exercise)
+
+      current_exercise_state = exercises_state[identifier] || []
+      new_state = Keyword.put(current_exercise_state, :status, :completed)
+
+      Workshop.State.update(:exercises, Keyword.put(exercises_state, identifier, new_state))
+      Workshop.State.persist!
+
+      Mix.shell.info "Marked as completed!"
     else
       Mix.shell.info "This command should get executed from within an exercise folder"
     end
