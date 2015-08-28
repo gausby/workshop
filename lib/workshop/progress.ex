bd0880f0b7fab9f2acf7ae86f32b7242127d6e10
Martin Gausby <martin@gausby.dk>
2015-08-28 14:31:45 +0200

    Reformatted the progress module

--- lib/workshop/progress.ex
+++ lib/workshop/progress.ex
@@ -10,8 +10,8 @@ defmodule Workshop.Progress do
   """
   @spec find_next(String.t, String.t) :: {:next, String.t} | :workshop_over
   def find_next(source_folder, sandbox_folder) do
-    in_source = source_folder |> File.ls! |> Enum.reject(&(String.starts_with?(&1, ".")))
-    in_sandbox = sandbox_folder |> File.ls! |> Enum.reject(&(String.starts_with?(&1, ".")))
+    in_source = source_folder |> File.ls! |> Enum.reject(&hidden_file/1)
+    in_sandbox = sandbox_folder |> File.ls! |> Enum.reject(&hidden_file/1)
     {checked_out, remaining} = find_checked_out_and_non_checked_out(in_source, in_sandbox)
 
     case find_first_non_passing_exercise(checked_out) do
@@ -27,6 +27,9 @@ defmodule Workshop.Progress do
     end
   end
 
+  defp hidden_file(file),
+    do: String.starts_with? file, "."
+
   defp find_first_non_passing_exercise(exercises) do
     exercises
     |> Stream.drop_while(&(Exercise.passes?(elem(&1, 1))))
@@ -41,22 +44,24 @@ defmodule Workshop.Progress do
   def find_checked_out_and_non_checked_out(source, sandbox_folder) do
     in_source = create_exercise_set(source)
 
-    in_sandbox = sandbox_folder
-                 |> Enum.reject(&(String.starts_with?(&1, ".")))
-                 |> Enum.filter(&(Regex.match?(~r/^\d+_[a-z][\w_]*$/, &1)))
-                 |> Enum.map(&strip_number_prefix/1)
-                 |> create_exercise_set
-                 |> HashSet.intersection(in_source) # skip folders not in source
+    in_sandbox =
+      sandbox_folder
+      |> Enum.filter(&(Regex.match?(~r/^\d+_[a-z][\w_]*$/, &1)))
+      |> Enum.map(&strip_number_prefix/1)
+      |> create_exercise_set
+      |> HashSet.intersection(in_source) # skip folders not in source
 
-    checked_out = HashSet.intersection(in_source, in_sandbox)
-                  |> HashSet.to_list
-                  |> Exercises.get_weights_from_name(source)
-                  |> Enum.sort
+    checked_out =
+      HashSet.intersection(in_source, in_sandbox)
+      |> HashSet.to_list
+      |> Exercises.get_weights_from_name(source)
+      |> Enum.sort
 
-    not_checked_out = HashSet.difference(in_source, in_sandbox)
-                      |> HashSet.to_list
-                      |> Exercises.get_weights_from_name(source)
-                      |> Enum.sort
+    not_checked_out =
+      HashSet.difference(in_source, in_sandbox)
+      |> HashSet.to_list
+      |> Exercises.get_weights_from_name(source)
+      |> Enum.sort
 
     {checked_out, not_checked_out}
   end
